032812db17be531f744d97453fc78f8b48cc3e41
M Hisham <hisham.khairul@sg.wilmar-intl.com>
2024-05-27 18:08:14 +0800

    TODO: see if this is even implementable

--- src/game/src/game/typestate/mod.rs
+++ src/game/src/game/typestate/mod.rs
@@ -3,14 +3,31 @@ pub(crate) mod transitions;
 pub(crate) mod actions;
 
 use phases::{GamePhase, HasGamePhase};
-use actions::GameActions;
+use actions::{DrawActions, GameActions};
 use transitions::GamePhaseTransitions;
 
 trait Game<G, P>: 
     HasGamePhase<P> 
     + GameActions
-    + GamePhaseTransitions<G, P>
 where
     G: HasGamePhase<P>,
     P: GamePhase,
 {}
+
+pub struct Rummy {}
+
+impl Game<G, P> for Rummy {
+
+}
+
+impl GameActions for Rummy {}
+
+impl DrawActions for Rummy {
+    fn draw_stock(&mut self) -> Result<(), String> {
+        todo!()
+    }
+
+    fn draw_discard_pile(&mut self) -> Result<(), String> {
+        todo!()
+    }
+}

dd0639ab3e578eaa361e9ee62741862b9e3bd1d1
M Hisham <hisham.khairul@sg.wilmar-intl.com>
2024-06-13 18:03:52 +0800

    mostly finished tests for Run

--- src/game/src/cards/tests/deck.rs
+++ src/game/src/cards/tests/deck.rs
@@ -5,10 +5,9 @@ use crate::cards::suit_rank::Rank;
 use super::super::deck::DeckConfig;
 use super::super::deck::Deck;
 
-/// Normal deck must be instantiated correctly.
 #[test]
-fn normal_deck() {
-    let mut cfg = DeckConfig {
+fn default_deck_config() {
+    let cfg = DeckConfig {
         shuffle_seed: None,
         pack_count: 1,
         high_rank: None,
@@ -17,32 +16,38 @@ fn normal_deck() {
 
     let default_cfg = DeckConfig::new();
     assert_eq!(cfg, default_cfg);
+}
 
+
+#[test]
+fn normal_deck() {
+    let cfg = DeckConfig::new();
     let deck = Deck::new(cfg.clone());
     assert_eq!(deck.stock().len(), 52);
     assert_eq!(deck.discard_pile().len(), 0);
-
-    // `use_joker` should add 2 jokers to the deck
-    let joker_deck = Deck::new(cfg.clone());
-    assert_eq!(joker_deck.stock().len(), 54);
-
-    // `pack_count = 0` should be set to 1 during Deck::new()
-    cfg.pack_count = 0; 
-    let zero_pack_deck = Deck::new(cfg.clone());
-    assert_eq!(zero_pack_deck.config().pack_count, 1);
-    assert_eq!(zero_pack_deck.stock().len(), 54);
 }
 
-/// Two pack deck must be instantiated correctly.
+#[test]
+fn joker_deck() {
+    let mut cfg = DeckConfig::new();
+    cfg.wildcard_rank = Some(Rank::Joker);
+    let joker_deck = Deck::new(cfg.clone());
+    assert_eq!(joker_deck.stock().len(), 54);
+}
+
+#[test]
+fn zero_pack_deck() {
+    let mut cfg = DeckConfig::new();
+    cfg.pack_count = 0; // should change to 1 when passed to a Deck::new()
+    let zero_pack_deck = Deck::new(cfg.clone());
+    assert_eq!(zero_pack_deck.config().pack_count, 1);
+    assert_eq!(zero_pack_deck.stock().len(), 52);
+}
+
 #[test]
 fn two_pack_deck() {
-    let mut cfg = DeckConfig {
-        shuffle_seed: None,
-        pack_count: 2,
-        high_rank: None,
-        wildcard_rank: None,
-    };
-
+    let mut cfg = DeckConfig::new();
+    cfg.pack_count = 2;
     let deck = Deck::new(cfg.clone());
     assert_eq!(deck.stock().len(), 104);
     assert_eq!(deck.discard_pile().len(), 0);
@@ -52,8 +57,6 @@ fn two_pack_deck() {
     assert_eq!(joker_deck.stock().len(), 108);
 }
 
-/// If the shuffle seed is `Some(0)`, the stock shouldn't be shuffled;
-/// ie, always in increasing order.
 #[test]
 fn no_shuffle_deck() {
     let cfg = DeckConfig {
@@ -64,13 +67,12 @@ fn no_shuffle_deck() {
     };
 
     let deck = Deck::new(cfg.clone());
-    assert!(deck.stock()
+    assert!(deck.stock() // didn't shuffle, so cards should be in increasing order
         .windows(2)
-        .all(|w| w[0] <= w[1])
+        .all(|w| w[0] < w[1])
     );
 }
 
-/// Draw and discard must work as expected.
 #[test]
 fn draw_and_discard_deck() {
     let mut deck = Deck::new(DeckConfig::new());
@@ -89,31 +91,32 @@ fn draw_and_discard_deck() {
 
     // Drawing from an empty stock
     assert!(deck.draw(1).is_err());
+}
 
-    // `shuffle_discarded` replenishes the stock but shuffles it
+#[test]
+fn shuffle_discarded_deck() {
+    let mut deck = Deck::new(DeckConfig::new());
+    let mut cards = deck.draw(52).unwrap();
+    deck.add_to_discard_pile(&mut cards);
     deck.shuffle_discarded();
+
     assert_eq!(deck.stock().len(), 52);
     assert_eq!(deck.discard_pile().len(), 0);
+}
 
-    // `turnover_discarded` replenishes the stock and preserves order (as no shuffle occurs);
-    // we can test by creating an unshuffled deck, calling `turnover_discarded` and checking the order
-    let mut unshuffled_deck = Deck::new(
-        DeckConfig {
-            shuffle_seed: Some(0),
-            pack_count: 1,
+#[test]
+fn turnover_discarded_deck() {
+    let mut cfg = DeckConfig::new();
+    cfg.shuffle_seed = Some(0); // set this seed so that we don't shuffle...
+    let mut deck = Deck::new(cfg);
+    let mut cards = deck.draw(52).unwrap();
+    deck.add_to_discard_pile(&mut cards);
+    deck.turnover_discarded();
 
-            high_rank: None,
-            wildcard_rank: None
-        }
-    );
-    let mut cards = unshuffled_deck.draw(52).unwrap();
-    unshuffled_deck.add_to_discard_pile(&mut cards);
-    unshuffled_deck.turnover_discarded();
-    println!("{:?}", unshuffled_deck.stock());
-    assert_eq!(unshuffled_deck.stock().len(), 52);
-    assert_eq!(unshuffled_deck.discard_pile().len(), 0);
-    assert!(unshuffled_deck.stock()
+    assert_eq!(deck.stock().len(), 52);
+    assert_eq!(deck.discard_pile().len(), 0);
+    assert!(deck.stock() // ... since we didn't shuffle, we can verify turnover by a strictly increasing order of cards
         .windows(2)
-        .all(|w| w[0] >= w[1])
+        .all(|w| w[0] > w[1])
     );
 }
